536c11b649fa490f257b78902c2a7f0fe0e2429b
artosalas <artosalas24@gmail.com>
2024-10-20 14:55:00 +0200

    Create artosalas.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/artosalas.java
@@ -0,0 +1,26 @@
+//sitio oficial de java https://www.java.com/es/
+
+// diferentes tipos para comentar /*
+
+public class Artos {
+ public static void main(String[] args) {
+
+//tipos de datos primitivos 
+
+string texto = hola; //cadena de texto
+int entero = 25; // entero
+boolean booleano = true; // booleano arroja  verdadero y falso
+boolean booleano = false
+double decimal = 7.77; // decimal de doble precision 
+float flotante = 7.56; // decimal de precision simple 
+char caracter = 'A'; // Carácter unico
+
+
+
+//imprimir por terminal 
+
+System.out.println("hola, Java");
+
+}
+}
+
